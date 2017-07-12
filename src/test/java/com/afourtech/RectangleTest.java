0b532d70b250cb48c71bbaaa6cd0cdc1f2f0ea83
root <root@sumitsalunke5.mylabserver.com>
2017-07-12 09:49:03 +0000

    adding testing files

---
+++ src/test/java/com/afourtech/RectangleTest.java
@@ -0,0 +1,30 @@
+package com.afourtech;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.afourtech.Rectangle;
+
+public class RectangleTest {
+  Rectangle myRectangle = new Rectangle(5,6);
+
+  @Test
+  public void testGetArea() {
+    assertEquals(myRectangle.getArea(), 30);
+  }
+
+  @Test
+  public void testGetPerimeter() {
+    assertEquals(myRectangle.getPerimeter(), 22);
+  }
+
+  @Test
+  public void testLength() {
+    assertEquals(myRectangle.length, 5);
+  }
+
+  @Test
+  public void testWidth() {
+    assertEquals(myRectangle.width, 6);
+  }
+}
