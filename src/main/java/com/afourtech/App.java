164c9a59a94091bd167c1edb2cc9b80d88ea9453
root <root@sumitsalunke5.mylabserver.com>
2017-07-12 06:02:52 +0000

    Adding to master

---
+++ src/main/java/com/afourtech/App.java
@@ -0,0 +1,13 @@
+package com.afourtech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
